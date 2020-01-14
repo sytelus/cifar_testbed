a88c6ad7924138d68672761bdc7ad719629ee845
Shital Shah <shitals@microsoft.com>
2020-01-14 08:19:23 -0800

    try1: fastai adamw setting

--- README.md
+++ README.md
@@ -6,4 +6,10 @@ pip install runstats
 ```
 
 ## Results
+ResNet original paper: 92.63%, ResNet32, 200 epochs
+SuperConvergence:
+    94%, 30 epochs
+    94%, 18 epochs with test time augmentations
 
+## Credits
+https://github.com/akamaster/pytorch_resnet_cifar10
--- main.py
+++ main.py
@@ -193,7 +193,7 @@ def train_test(exp_name:str, exp_desc:str, epochs:int, model_name:str,
         logging.info(f'optim_type={optim_type}, '
                      f'lr={lr}, momentum={momentum}, weight_decay={weight_decay}')
     elif optim_type=='sc': # super convergence
-        lr, betas, weight_decay = 0.00078125, (0.9, 0.999), 0.0005
+        lr, betas, weight_decay = 3.0e-3, (0.95,0.85), 1.2e-6
         optim = torch.optim.AdamW(net.parameters(), lr=lr, betas=betas, eps=1.0e-08,
                           weight_decay=weight_decay, amsgrad=False)
         logging.info(f'optim_type={optim_type}, '

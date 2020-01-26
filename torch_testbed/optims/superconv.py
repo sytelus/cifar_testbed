b4c65689b8a0b23dcda9939669a2ad625dbc40cf
Shital Shah <shitals@microsoft.com>
2020-01-26 01:31:10 -0800

    lr schedule

--- torch_testbed/optims/superconv.py
+++ torch_testbed/optims/superconv.py
@@ -5,9 +5,9 @@ def optim_sched(epochs, net, *kargs, **kvargs):
         batch_size, train_size = 512, 50000
         steps_per_epoch = round(train_size / batch_size)
         total_steps = steps_per_epoch * epochs
-        warmup_steps = steps_per_epoch * 5 # first 5 epochs
+        warmup_steps = steps_per_epoch * 15 # first 15 epochs
 
-        lr, momentum, weight_decay = 0.4, 0.9, 0.000125 * 512
+        lr, momentum, weight_decay = 0.1, 0.9, 5e-4*batch_size
         optim = torch.optim.SGD(net.parameters(),
                                 lr, momentum=momentum, weight_decay=weight_decay)
         logging.info(f'lr={lr}, momentum={momentum}, weight_decay={weight_decay}')

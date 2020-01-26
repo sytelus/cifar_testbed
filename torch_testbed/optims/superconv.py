45a44374fdebd8d02e29e5481c18d7ee7106c05e
Shital Shah <shitals@microsoft.com>
2020-01-26 01:46:03 -0800

    adj lr

--- torch_testbed/optims/superconv.py
+++ torch_testbed/optims/superconv.py
@@ -7,13 +7,13 @@ def optim_sched(epochs, net, *kargs, **kvargs):
         total_steps = steps_per_epoch * epochs
         warmup_steps = steps_per_epoch * 15 # first 15 epochs
 
-        lr, momentum, weight_decay = 0.44, 0.9, 5e-4*batch_size
+        lr, momentum, weight_decay = 0.1, 0.9, 5e-4
         optim = torch.optim.SGD(net.parameters(),
                                 lr, momentum=momentum, weight_decay=weight_decay)
         logging.info(f'lr={lr}, momentum={momentum}, weight_decay={weight_decay}')
 
         sched = torch.optim.lr_scheduler.OneCycleLR(
-            optim, max_lr=lr/batch_size, epochs=epochs, steps_per_epoch=steps_per_epoch,
+            optim, max_lr=lr, epochs=epochs, steps_per_epoch=steps_per_epoch,
             pct_start=warmup_steps/total_steps, anneal_strategy='linear',
             cycle_momentum=False, div_factor=100000.0,
             final_div_factor=100000.0

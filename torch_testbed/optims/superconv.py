6496e875e31cb32dcb91153ccf9c79c6d98adaed
Shital Shah <shitals@microsoft.com>
2020-01-26 01:41:57 -0800

    fix lr

--- torch_testbed/optims/superconv.py
+++ torch_testbed/optims/superconv.py
@@ -13,11 +13,11 @@ def optim_sched(epochs, net, *kargs, **kvargs):
         logging.info(f'lr={lr}, momentum={momentum}, weight_decay={weight_decay}')
 
         sched = torch.optim.lr_scheduler.OneCycleLR(
-            optim, max_lr=lr, epochs=epochs, steps_per_epoch=steps_per_epoch,
+            optim, max_lr=lr/batch_size, epochs=epochs, steps_per_epoch=steps_per_epoch,
             pct_start=warmup_steps/total_steps, anneal_strategy='linear',
             cycle_momentum=False, div_factor=100000.0,
             final_div_factor=100000.0
         )
         sched_on_epoch = False
 
-        return optim, sched, sched_on_epoch, 512
+        return optim, sched, sched_on_epoch, batch_size

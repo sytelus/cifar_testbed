cb4c3fab4722184e4adf2d54b7a9499ce0e2f07b
Shital Shah <shitals@microsoft.com>
2020-01-26 00:57:09 -0800

    fix layer sizes

--- torch_testbed/cifar10_models/davidnet.py
+++ torch_testbed/cifar10_models/davidnet.py
@@ -54,11 +54,11 @@ class DavidNet(nn.Module):
         assert not pretrained
         self._op =  nn.Sequential(
             ConvBn(3, 64),
-            ConvRes(64, 64*2),
+            ConvRes(64, 64),
             ConvBnPool(64*2, 64*4),
-            ConvRes(64*4, 64*8),
-            nn.AdaptiveMaxPool2d(512),
-            nn.Linear(in_features=512, out_features=10, bias=True)
+            ConvRes(64*4, 64*4),
+            nn.AdaptiveMaxPool2d(128),
+            nn.Linear(in_features=128, out_features=10, bias=True)
         )
 
     def forward(self, x):
